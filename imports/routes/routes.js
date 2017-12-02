ef8fc14de84f398fd7f32995fe3a46aa36263ff9
Hani Elmalky <hani.elmalky@gmail.com>
2017-12-02 15:22:57 -0800

    refactor router to allow for dynamic urls

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -7,41 +7,24 @@ import Dashboard from '../ui/Dashboard';
 import NotFound from '../ui/NotFound';
 import Login from '../ui/Login';
 import { Session } from 'meteor/session';
+import { log } from 'util';
 
-//? Check below line as it doesn't exist in Andrew's version!
-//window.browserHistory = browserHistory;
-
-const unauthenticatedPages = ['/', '/signup'];
-const authenticatedPages = ['/dashboard'];
-const onEnterPublicPage = () => {
-  if (Meteor.userId()) {
-    browserHistory.replace('/dashboard');
-  }
-};
-
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
 
 const onEnterNotePage = (nextState) => {
-  //* nextState is a property in Meteor that you can use to inquiry information about loaded page.
+  //* nextState is a property in Meteor that we can use to inquiry information about loaded page.
   //* use console.log(nextState) to know what is inside
-  
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  } else {
-    Session.set('selectedNoteId',nextState.params.id);
-  }
+  //* prevState is a property in Meteor that we can use to inquiry information about previous page(s) !!check (s)
+
+  Session.set('selectedNoteId', nextState.params.id);
 };
 
-export const onAuthChange = (isAuthenticated) => {
-  const pathname = browserHistory
-    .getCurrentLocation()
-    .pathname;
-  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+const onLeaveNotePage = () => {
+  Session.set('selectedNoteId',undefined);
+};
+
+export const onAuthChange = (isAuthenticated, currentPagePrivacy) => {
+  const isUnauthenticatedPage = currentPagePrivacy === 'unauthenticated' ;
+  const isAuthenticatedPage = currentPagePrivacy === 'authenticated' ;
 
   if (isUnauthenticatedPage && isAuthenticated) {
     browserHistory.replace('/dashboard');
@@ -50,14 +33,27 @@ export const onAuthChange = (isAuthenticated) => {
   }
 };
 
+export const globalOnChange = (prevState, nextState) => {
+  globalOnEnter(nextState);
+};
+
+export const globalOnEnter = (nextState) => {
+  const lastRoute = nextState.routes[nextState.routes.length - 1];
+  Session.set('currentPagePrivacy', lastRoute.privacy);
+  
+};
+
 export const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterNotePage} />
-    <Route path="*" component={NotFound}/>
+    <Route onEnter={globalOnEnter} onChange={globalOnChange} >
+      <Route path="/" component={Login} privacy="unauthenticated"  />
+      <Route path="/signup" component={Signup} privacy="unauthenticated"  />
+      <Route path="/dashboard" component={Dashboard} privacy="authenticated" />
+      <Route path="/dashboard/:id" component={Dashboard} privacy="authenticated" onEnter={onEnterNotePage} onLeave={onLeaveNotePage}/>
+      <Route path="*" component={NotFound} />
+    </Route>
   </Router>
 );
 
 //* Use of /:id will allow us to fetch ID from variable id
+//* privacy is something we made up, there is no such property in Route/Routes/Router
